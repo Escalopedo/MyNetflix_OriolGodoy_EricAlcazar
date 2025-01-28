7b0cb707454838106c9907b63c8c2c92c2f824aa
PC-ERIC <eric.alcazarc@gmail.com>
2025-01-28 17:52:26 +0100

    Inserts iniciales a la bbdd

---
+++ sql/inserts.sql
@@ -0,0 +1,13 @@
+INSERT INTO usuarios (rol, nombre, apellido, correo, contrasena, telefono) VALUES
+  ('admin', 'Eric', 'Alcazar', 'eric@gmail.com', 'qweQWE123', '611475124'),
+  ('admin', 'Ori', 'Godoy', 'ori@gmail.com', 'qweQWE123', '619421222'),
+  ('cliente', 'Alberto', 'DeSantos', 'alberto@gmail.com', 'qweQWE123', '600000001'),
+  ('cliente', 'Fatima', 'Martinez', 'fatima@gmail.com', 'qweQWE123', '600000002'),
+  ('cliente', 'Agnes', 'Plans', 'agnes@gmail.com', 'qweQWE123', '600000003');
+
+INSERT INTO carteleras (titulo, descripcion, img) VALUES
+  ('Batman Begins', 'El origen de Batman, donde Bruce Wayne se convierte en el Caballero Oscuro para luchar contra el crimen en Gotham City.', 'batman_begins.jpg'),
+  ('The Dark Knight', 'Batman enfrenta al Joker, un criminal psicópata que busca sumergir a Gotham City en el caos.', 'the_dark_knight.jpg'),
+  ('The Dark Knight Rises', 'Después de una larga ausencia, Batman regresa para enfrentarse a Bane, un terrorista que planea destruir Gotham City.', 'the_dark_knight_rises.jpg'),
+  ('Batman v Superman: Dawn of Justice', 'Batman se enfrenta a Superman, creyendo que es una amenaza para la humanidad, mientras se prepara la llegada de nuevos héroes.', 'batman_v_superman.jpg'),
+  ('The Batman', 'Un joven Bruce Wayne investiga una serie de crímenes en Gotham mientras enfrenta a Enigma y otros villanos.', 'the_batman.jpg');
