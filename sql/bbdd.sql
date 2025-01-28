c634362d1b6005e7886cda8f873fad7ba0cf220d
PC-ERIC <eric.alcazarc@gmail.com>
2025-01-28 17:39:50 +0100

    BBDD primer boceto

---
+++ sql/bbdd.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE netflix;
+USE netflix;
+
+CREATE TABLE usuarios (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  rol enum('cliente','admin') NOT NULL,
+  nombre varchar(20) NOT NULL,
+  apellido varchar(30) NOT NULL,
+  correo varchar(100) NOT NULL UNIQUE,
+  contrasena varchar(255) NOT NULL,
+  telefono char(9) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE carteleras (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  titulo varchar(40) NOT NULL,
+  descripcion varchar(500) NOT NULL,
+  img varchar(155) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE likes (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  id_usuarios int(11) NOT NULL,
+  id_carteleras int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY id_carteleras (id_carteleras),
+  KEY id_usuarios (id_usuarios)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- FKs
+
+ALTER TABLE likes
+  ADD CONSTRAINT fk_likes_usuarios
+  FOREIGN KEY (id_usuarios) REFERENCES usuarios (id);
+
+ALTER TABLE likes
+  ADD CONSTRAINT fk_likes_carteleras
+  FOREIGN KEY (id_carteleras) REFERENCES carteleras (id);
