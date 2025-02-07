ec68d846d30d6335339a198fb4a2085adc7a158d
PC-ERIC <eric.alcazarc@gmail.com>
2025-02-07 17:18:07 +0100

    Redireccion con js

---
+++ js/adminAjax.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Control de navegación entre secciones
+    $(".nav-link").click(function () {
+        var seccion = $(this).data("seccion");  // Obtener el nombre de la sección
+        $(".seccion").hide();  // Ocultar todas las secciones
+        $("#" + seccion).show();  // Mostrar solo la sección seleccionada
+    });
+
+    // Mostrar la primera sección por defecto (Usuarios)
+    $(".nav-link[data-seccion='usuarios']").click();
+});
